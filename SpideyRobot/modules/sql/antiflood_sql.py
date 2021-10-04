407d2e643724dc5df36f7f039aec66051b51e651
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 16:27:06 +0530

    🕷 sql

--- SpideyRobot/modules/sql/antiflood_sql.py
+++ SpideyRobot/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, UnicodeText
 
-from GreysonBot.modules.sql import SESSION, BASE
+from SpideyRobot.modules.sql import SESSION, BASE
 
 DEF_COUNT = 1
 DEF_LIMIT = 0
