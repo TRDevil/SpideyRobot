7342e94eef5e95aa5383a7e7d92d29c7161f795c
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 16:36:09 +0530

    🕷 sql

--- SpideyRobot/modules/sql/reporting_sql.py
+++ SpideyRobot/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from GreysonBot.modules.sql import BASE, SESSION
+from SpideyRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String
 
 
