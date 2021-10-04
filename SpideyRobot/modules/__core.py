8e7a9930002b9c97c5bcc43f8eafb27b72ff06e4
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 14:38:44 +0530

    🕷 Modules

--- SpideyRobot/modules/__core.py
+++ SpideyRobot/modules/__core.py
@@ -1,15 +1,15 @@
-from GreysonBot import telethn as tbot
-from GreysonBot.events import register
+from SpideyRobot import telethn as tbot
+from SpideyRobot.events import register
 import os
 import asyncio
 import os
 import time
 from datetime import datetime
-from GreysonBot import OWNER_ID, DEV_USERS
-from GreysonBot import TEMP_DOWNLOAD_DIRECTORY as path
-from GreysonBot import TEMP_DOWNLOAD_DIRECTORY
+from SpideyRobot import OWNER_ID, DEV_USERS
+from SpideyRobot import TEMP_DOWNLOAD_DIRECTORY as path
+from SpideyRobot import TEMP_DOWNLOAD_DIRECTORY
 from datetime import datetime
-water = './GreysonBot/resources/yone.jpg'
+water = './SpideyRobot/resources/Spideybg.png'
 client = tbot
 
 @register(pattern=r"^/send ?(.*)")
@@ -21,7 +21,7 @@ async def Prof(event):
     thumb = water
     message_id = event.message.id
     input_str = event.pattern_match.group(1)
-    the_plugin_file = "./GreysonBot/modules/{}.py".format(input_str)
+    the_plugin_file = "./SpideyRobot/modules/{}.py".format(input_str)
     if os.path.exists(the_plugin_file):
      message_id = event.message.id
      await event.client.send_file(
