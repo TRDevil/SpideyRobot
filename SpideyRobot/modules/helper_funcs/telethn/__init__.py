3d6ee3dbf8f9f7ea48fc0a5b618190cc1dd3a5de
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 16:15:19 +0530

    🕷 Telethn

--- SpideyRobot/modules/helper_funcs/telethn/__init__.py
+++ SpideyRobot/modules/helper_funcs/telethn/__init__.py
@@ -1,4 +1,4 @@
-from GreysonBot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES, telethn
+from SpideyRobot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES, telethn
 
 IMMUNE_USERS = DRAGONS + WOLVES + DEMONS + TIGERS + DEV_USERS
 
