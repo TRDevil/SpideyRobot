d3816a4ff388002a560667f46571e8333389082e
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 16:28:08 +0530

    🕷 sql

--- SpideyRobot/modules/sql/blacklist_sql.py
+++ SpideyRobot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from GreysonBot.modules.sql import SESSION, BASE
+from SpideyRobot.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
