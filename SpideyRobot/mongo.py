16695d200d6b2577170de3ad9ec019175796d329
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 13:25:24 +0530

    🕷 mongo.py

--- SpideyRobot/mongo.py
+++ SpideyRobot/mongo.py
@@ -21,4 +21,4 @@ db = client["SpideyRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
-    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
+    sys.exit(log.critical("Can't connect to mongodb! Exiting.."))
