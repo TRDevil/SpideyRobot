5f33daceeec83d21a053d9b6f6be94396bfd70b6
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 15:24:28 +0530

    🕷 Modules

--- SpideyRobot/modules/currency_converter.py
+++ SpideyRobot/modules/currency_converter.py
@@ -1,5 +1,5 @@
 import requests
-from GreysonBot import CASH_API_KEY, dispatcher
+from SpideyRobot import CASH_API_KEY, dispatcher
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
