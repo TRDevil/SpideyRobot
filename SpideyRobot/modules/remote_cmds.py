0444dadcace1832bb9c1a529f4d0a99e15b9e845
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 15:55:25 +0530

    🕷 Modules

--- SpideyRobot/modules/remote_cmds.py
+++ SpideyRobot/modules/remote_cmds.py
@@ -1,12 +1,12 @@
-from GreysonBot import dispatcher
-from GreysonBot.modules.helper_funcs.chat_status import (
+from SpideyRobot import dispatcher
+from SpideyRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from GreysonBot.modules.helper_funcs.extraction import extract_user_and_text
-from GreysonBot.modules.helper_funcs.filters import CustomFilters
+from SpideyRobot.modules.helper_funcs.extraction import extract_user_and_text
+from SpideyRobot.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
