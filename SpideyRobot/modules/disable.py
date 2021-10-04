a0bb6066acbfae18a6864d29035316aa6823858a
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 15:27:39 +0530

    🕷 Modules

--- SpideyRobot/modules/disable.py
+++ SpideyRobot/modules/disable.py
@@ -2,9 +2,9 @@ import importlib
 from typing import Union
 
 from future.utils import string_types
-from GreysonBot import dispatcher
-from GreysonBot.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
-from GreysonBot.modules.helper_funcs.misc import is_module_loaded
+from SpideyRobot import dispatcher
+from SpideyRobot.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
+from SpideyRobot.modules.helper_funcs.misc import is_module_loaded
 from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
@@ -20,12 +20,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from GreysonBot.modules.helper_funcs.chat_status import (
+    from SpideyRobot.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
     )
-    from GreysonBot.modules.sql import disable_sql as sql
+    from SpideyRobot.modules.sql import disable_sql as sql
     from telegram.ext.dispatcher import run_async
 
     DISABLE_CMDS = []
@@ -160,7 +160,7 @@ if is_module_loaded(FILENAME):
         args = context.args
         chat = update.effective_chat
         if len(args) >= 1:
-            disable_module = "GreysonBot.modules." + args[0].rsplit(".", 1)[0]
+            disable_module = "SpideyRobot.modules." + args[0].rsplit(".", 1)[0]
 
             try:
                 module = importlib.import_module(disable_module)
@@ -235,7 +235,7 @@ if is_module_loaded(FILENAME):
         chat = update.effective_chat
 
         if len(args) >= 1:
-            enable_module = "GreysonBot.modules." + args[0].rsplit(".", 1)[0]
+            enable_module = "SpideyRobot.modules." + args[0].rsplit(".", 1)[0]
 
             try:
                 module = importlib.import_module(enable_module)
