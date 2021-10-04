c69f65b9f01876c1e7d318daa616daadbcc523e6
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 16:37:10 +0530

    🕷 sql

--- SpideyRobot/modules/sql/userinfo_sql.py
+++ SpideyRobot/modules/sql/userinfo_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, Integer, UnicodeText
 
-from GreysonBot.modules.sql import SESSION, BASE
+from SpideyRobot.modules.sql import SESSION, BASE
 
 
 class UserInfo(BASE):
