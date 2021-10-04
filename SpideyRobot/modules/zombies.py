eb5617aeca390aa4db4e683d471c976e4af2b144
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 16:12:40 +0530

    🕷 Modules

--- SpideyRobot/modules/zombies.py
+++ SpideyRobot/modules/zombies.py
@@ -6,7 +6,7 @@ from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
 
-from GreysonBot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
+from SpideyRobot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
 
 # =================== CONSTANT ===================
 
@@ -114,7 +114,7 @@ async def zombies(event):
     
     
 from telethon.tl.types import UserStatusLastMonth, UserStatusLastWeek, ChatBannedRights
-from GreysonBot.events import register
+from SpideyRobot.events import register
 from telethon import *
 from telethon.tl.functions.channels import (EditBannedRequest)
                                             
