430fee7386bde105b78e85933dd48571ee6e367f
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 16:06:52 +0530

    🕷 Modules

--- SpideyRobot/modules/wallpaper.py
+++ SpideyRobot/modules/wallpaper.py
@@ -1,12 +1,10 @@
 from random import randint
 
 import requests as r
-from GreysonBot import SUPPORT_CHAT, WALL_API, dispatcher
-from GreysonBot.modules.disable import DisableAbleCommandHandler
+from SpideyRobot import SUPPORT_CHAT, WALL_API, dispatcher
+from SpideyRobot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
-
-# Wallpapers module by @TheRealPhoenix using wall.alphacoders.com
 
 
 @run_async
