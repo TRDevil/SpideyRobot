2f156045e837bb1cf07160420cedbefe00fb9278
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 13:03:30 +0530

    🕷 Setup venv.bat

--- Setup venv.bat
+++ Setup venv.bat
@@ -3,4 +3,4 @@ TITLE Setting up virtual env
 py -m venv env && env\scripts\activate.bat && pip install -r requirements.txt
 
 :: Note to rerun the requirements.txt in case you ever add a mdoule.
-:: Running this multiple time will not make a mess of your setup, dont worry about that bit.
+:: Running this multiple time will not make a mess of your setup, don't worry about that bit.
