c258858e8ee19f0c75959d775017d39cb93431c4
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 15:01:20 +0530

    🕷 Modules

--- SpideyRobot/modules/bans.py
+++ SpideyRobot/modules/bans.py
@@ -5,7 +5,7 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from GreysonBot import (
+from SpideyRobot import (
     DEV_USERS,
     LOGGER,
     OWNER_ID,
@@ -15,8 +15,8 @@ from GreysonBot import (
     WOLVES,
     dispatcher,
 )
-from GreysonBot.modules.disable import DisableAbleCommandHandler
-from GreysonBot.modules.helper_funcs.chat_status import (
+from SpideyRobot.modules.disable import DisableAbleCommandHandler
+from SpideyRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
@@ -27,9 +27,9 @@ from GreysonBot.modules.helper_funcs.chat_status import (
     user_can_ban,
     can_delete,
 )
-from GreysonBot.modules.helper_funcs.extraction import extract_user_and_text
-from GreysonBot.modules.helper_funcs.string_handling import extract_time
-from GreysonBot.modules.log_channel import gloggable, loggable
+from SpideyRobot.modules.helper_funcs.extraction import extract_user_and_text
+from SpideyRobot.modules.helper_funcs.string_handling import extract_time
+from SpideyRobot.modules.log_channel import gloggable, loggable
 
 
 @run_async
