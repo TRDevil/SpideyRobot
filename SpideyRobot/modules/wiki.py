c7c7ea513213b95e08dfecc379c5fc1921a1c3d4
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 16:11:22 +0530

    🕷 Modules

--- SpideyRobot/modules/wiki.py
+++ SpideyRobot/modules/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
-from GreysonBot import dispatcher
-from GreysonBot.modules.disable import DisableAbleCommandHandler
+from SpideyRobot import dispatcher
+from SpideyRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
