c05c109a438988ac914f474ed3da2f0f487977db
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 16:08:19 +0530

    🕷 Modules

--- SpideyRobot/modules/warns.py
+++ SpideyRobot/modules/warns.py
@@ -3,9 +3,9 @@ import re
 from typing import Optional
 
 import telegram
-from GreysonBot import TIGERS, WOLVES, dispatcher
-from GreysonBot.modules.disable import DisableAbleCommandHandler
-from GreysonBot.modules.helper_funcs.chat_status import (
+from SpideyRobot import TIGERS, WOLVES, dispatcher
+from SpideyRobot.modules.disable import DisableAbleCommandHandler
+from SpideyRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     is_user_admin,
@@ -14,16 +14,16 @@ from GreysonBot.modules.helper_funcs.chat_status import (
     user_admin_no_reply,
     can_delete,
 )
-from GreysonBot.modules.helper_funcs.extraction import (
+from SpideyRobot.modules.helper_funcs.extraction import (
     extract_text,
     extract_user,
     extract_user_and_text,
 )
-from GreysonBot.modules.helper_funcs.filters import CustomFilters
-from GreysonBot.modules.helper_funcs.misc import split_message
-from GreysonBot.modules.helper_funcs.string_handling import split_quotes
-from GreysonBot.modules.log_channel import loggable
-from GreysonBot.modules.sql import warns_sql as sql
+from SpideyRobot.modules.helper_funcs.filters import CustomFilters
+from SpideyRobot.modules.helper_funcs.misc import split_message
+from SpideyRobot.modules.helper_funcs.string_handling import split_quotes
+from SpideyRobot.modules.log_channel import loggable
+from SpideyRobot.modules.sql import warns_sql as sql
 from telegram import (
     CallbackQuery,
     Chat,
@@ -45,7 +45,7 @@ from telegram.ext import (
     run_async,
 )
 from telegram.utils.helpers import mention_html
-from GreysonBot.modules.sql.approve_sql import is_approved
+from SpideyRobot.modules.sql.approve_sql import is_approved
 
 WARN_HANDLER_GROUP = 9
 CURRENT_WARNING_FILTER_STRING = "<b>Current warning filters in this chat:</b>\n"
