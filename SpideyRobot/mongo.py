81fbb530c9def7df87d7ddaa03c22619526c73a3
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 13:24:41 +0530

    Update mongo.py

--- SpideyRobot/mongo.py
+++ SpideyRobot/mongo.py
@@ -2,10 +2,10 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from GreysonBot import MONGO_DB_URI 
+from SpideyRobot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from GreysonBot.conf import get_int_key, get_str_key
+from SpideyRobot.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
@@ -17,7 +17,7 @@ client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["GreysonBot"]
+db = client["SpideyRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
