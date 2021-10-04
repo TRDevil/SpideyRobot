ac732120a24b9c1c315d7f74a78e16a24d63fca7
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 16:25:27 +0530

    🕷 sql

--- SpideyRobot/modules/sql/__init__.py
+++ SpideyRobot/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from GreysonBot import DB_URI
+from SpideyRobot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
