1abb8ab729909b77a387e158690f3b853aed8ff3
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 16:22:56 +0530

    🕷 helper_funcs

--- SpideyRobot/modules/helper_funcs/msg_types.py
+++ SpideyRobot/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from GreysonBot.modules.helper_funcs.string_handling import button_markdown_parser
+from SpideyRobot.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
