231dc83523bce69b75051c810679ea3be57e1eb1
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 15:23:49 +0530

    🕷 Modules

--- SpideyRobot/modules/cricketscore.py
+++ SpideyRobot/modules/cricketscore.py
@@ -2,7 +2,7 @@ import urllib.request
 
 from bs4 import BeautifulSoup
 from telethon import events
-from GreysonBot import telethn as tbot
+from SpideyRobot import telethn as tbot
 from telethon.tl import functions, types
 from telethon.tl.types import *
 
