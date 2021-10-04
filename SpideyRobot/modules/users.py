f3c98834d7150ebf5fc833a480a8f0634b0cad0c
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 16:03:18 +0530

    🕷 Modules

--- SpideyRobot/modules/users.py
+++ SpideyRobot/modules/users.py
@@ -11,10 +11,10 @@ from telegram.ext import (
     run_async,
 )
 
-import GreysonBot.modules.sql.users_sql as sql
-from GreysonBot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from GreysonBot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from GreysonBot.modules.sql.users_sql import get_all_users
+import SpideyRobot.modules.sql.users_sql as sql
+from SpideyRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from SpideyRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from SpideyRobot.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
