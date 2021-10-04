2d4cafb17466e768df4588467102b92aff5bb172
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 14:53:08 +0530

    🕷 Modules

--- SpideyRobot/modules/admin.py
+++ SpideyRobot/modules/admin.py
@@ -5,9 +5,9 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from GreysonBot import DRAGONS, dispatcher
-from GreysonBot.modules.disable import DisableAbleCommandHandler
-from GreysonBot.modules.helper_funcs.chat_status import (
+from SpideyRobot import DRAGONS, dispatcher
+from SpideyRobot.modules.disable import DisableAbleCommandHandler
+from SpideyRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_pin,
     can_promote,
@@ -15,19 +15,19 @@ from GreysonBot.modules.helper_funcs.chat_status import (
     user_admin,
     ADMIN_CACHE,
 )
-from GreysonBot.helper_extra.admin_rights import (
+from SpideyRobot.helper_extra.admin_rights import (
     user_can_pin,
     user_can_promote,
     user_can_changeinfo,
 )
 
-from GreysonBot.modules.helper_funcs.extraction import (
+from SpideyRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from GreysonBot.modules.log_channel import loggable
-from GreysonBot.modules.helper_funcs.alternate import send_message
-from GreysonBot.modules.helper_funcs.alternate import typing_action
+from SpideyRobot.modules.log_channel import loggable
+from SpideyRobot.modules.helper_funcs.alternate import send_message
+from SpideyRobot.modules.helper_funcs.alternate import typing_action
 
 
 @run_async
