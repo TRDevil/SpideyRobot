c09ddd6618bebbfcea740f71256c2f3b2153867d
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-10-04 12:57:46 +0530

    🕷 exp.sh

--- exp.sh
+++ exp.sh
@@ -1,2 +1,2 @@
 sudo bash -c 'echo "{ \"cgroup-parent\": \"/actions_job\",\"experimental\":true}" > /etc/docker/daemon.json'
-sudo systemctl restart docker.service
+sudo systemctl restart docker.servic
